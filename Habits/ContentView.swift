e24464d82e78daecd689e228df4f4651d9fb044c
ShreyasTantri <shreyastantrik29@gmail.com>
2026-01-17 11:33:44 +0530

    Initial Commit

---
+++ Habits/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Habits
+//
+//  Created by CCS038 on 17/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Habits/HabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitsApp.swift
+//  Habits
+//
+//  Created by CCS038 on 17/01/26.
+//
+
+import SwiftUI
+
+@main
+struct HabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
